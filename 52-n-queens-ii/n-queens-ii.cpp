c1a0757d6b8e29fda83fe1d9da778271ac564199
shreelekha24 <2023ugee027@nitjsr.ac.in>
2025-04-04 12:01:55 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (19.44%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+void display(int col,vector<vector<string>>&v,vector<string>&temp,vector<int>&leftrow,vector<int>&uld,vector<int>&lld,int n)
+{
+    if(col==n)
+    {
+        v.push_back(temp);
+        return;
+    }
+    for(int row=0;row<n;row++)
+    {
+        if(leftrow[row]==0 && uld[n-1+col-row]==0 && lld[col+row]==0)
+        {
+            temp[row][col]='Q';
+            leftrow[row]=1;
+            uld[n-1+col-row]=1;
+            lld[col+row]=1;
+            display(col+1,v,temp,leftrow,uld,lld,n);
+            temp[row][col]='.';
+            leftrow[row]=0;
+            uld[n-1+col-row]=0;
+            lld[col+row]=0;
+        }
+    }
+}
+    int totalNQueens(int n) {
+        vector<vector<string>> v;
+    vector<string> temp(n, string(n, '.'));
+    vector<int>leftrow(n,0);
+    vector<int>uld(2*n-1,0);
+    vector<int>lld(2*n-1,0);
+    display(0, v, temp,leftrow,uld,lld,n);
+    return v.size();
+    }
+};
