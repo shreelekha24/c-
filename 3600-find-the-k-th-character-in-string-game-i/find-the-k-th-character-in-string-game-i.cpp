7d7856fc69c7b73f4016b661354e1b2a64e38dcf
shreelekha24 <2023ugee027@nitjsr.ac.in>
2025-06-03 12:18:18 +0530

    Time: 3 ms (57.40%) | Memory: 9 MB (78.55%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
+void solve(string& word, int k)
+{
+    if(word.size()>=k)
+    return;
+    int n=word.size();
+    for(int i=0;i<n;i++)
+    {
+        if(word[i]=='z')
+        word+='a';
+        else
+        word+=char(word[i]+1);
+    }
+   solve(word,k);
+}
 char kthCharacter(int k) {
-        string word="a";
-        while(word.size()<k){
-            int n=word.size();
-        for(int i=0;i<n && word.size()<k;i++)
-        {
-          if(word[i]=='z')
-          word+='a';
-          else
-          word+=char(word[i]+1);
-        }
-       }
+    string word="a";
+    solve(word,k);
     return word[k-1];
     }
 };
