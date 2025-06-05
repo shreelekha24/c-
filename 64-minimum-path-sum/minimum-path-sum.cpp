1de339cc4a79c3f6357c2b5b438ab5691550e13e
shreelekha24 <2023ugee027@nitjsr.ac.in>
2025-06-05 17:17:23 +0530

    Time: 1 ms (41.78%) | Memory: 16.3 MB (58.14%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -17,7 +17,23 @@ int solve(vector<vector<int>>&grid,int i,int j,int m,int n,vector<vector<int>>&d
         int m=grid.size();
         int n=grid[0].size();
         vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        int count=solve(grid,0,0,m,n,dp);
-        return count;
+     //   int count=solve(grid,0,0,m,n,dp);
+        dp[0][0]=0;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                  if(i==0 && j==0)
+                  dp[0][0]=grid[0][0];
+                  else{
+                    int down=INT_MAX,left=INT_MAX;
+                    if(i>0)
+                    down=grid[i][j]+dp[i-1][j];
+                    if(j>0)  left=grid[i][j]+dp[i][j-1];
+                    dp[i][j]=min(down,left);
+                  }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
