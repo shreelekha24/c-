272b597398bd19a2f9c1e76b5ad2b4d4fe915a0b
shreelekha24 <2023ugee027@nitjsr.ac.in>
2025-06-05 19:09:00 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (30.61%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -12,7 +12,27 @@ int solve(vector<vector<int>>& triangle,int i,int j,vector<vector<int>>& dp)
 }
     int minimumTotal(vector<vector<int>>& triangle) {
         vector<vector<int>>dp(triangle.size()+1,vector<int>(triangle.size()+1,-1));
-       int count=solve(triangle,0,0,dp);
-       return count; 
+       //int count=solve(triangle,0,0,dp);
+       int m;
+       dp[0][0]=triangle[0][0];
+       for(int i=1;i<triangle.size();i++)
+       {
+        m=triangle[i].size();
+        for(int j=0;j<triangle[i].size();j++)
+        {
+                int one=INT_MAX,two=INT_MAX;
+                if(j<triangle[i-1].size())
+                one=triangle[i][j]+dp[i-1][j];
+                if(j>0)
+                two=triangle[i][j]+dp[i-1][j-1];
+                dp[i][j]=min(one,two);
+            }
+        }
+        int ans = INT_MAX;
+    for (int j = 0; j < triangle.back().size(); j++) {
+        ans = min(ans, dp[triangle.size() - 1][j]);
+    }
+
+    return ans;
     }
 };
