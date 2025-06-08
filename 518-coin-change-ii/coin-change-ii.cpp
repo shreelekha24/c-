60c7eb51f236c65e243f46985f7a29c975ce7be5
shreelekha24 <2023ugee027@nitjsr.ac.in>
2025-06-08 19:50:10 +0530

    Time: 17 ms (61.57%) | Memory: 28.2 MB (60.08%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+int mod=1e9+7;
 int solve(vector<int>&coins,int amount,int i,vector<vector<int>>&dp)
 {
     if(i==0)
@@ -15,8 +16,23 @@ int solve(vector<int>&coins,int amount,int i,vector<vector<int>>&dp)
     return dp[i][amount]=take;
 }
     int change(int amount, vector<int>& coins) {
-        vector<vector<int>>dp(coins.size(),vector<int>(amount+1,-1));
-        int count=solve(coins,amount,coins.size()-1,dp);
-        return count;
+        vector<vector<int>>dp(coins.size(),vector<int>(amount+1,0));
+    //    int count=solve(coins,amount,coins.size()-1,dp);
+        for (int i = 0; i < coins.size(); i++) dp[i][0] = 1;
+        for(int i=1;i<=amount;i++)
+        {
+            if(i%coins[0]==0) dp[0][i]=1;
+        }
+        for(int i=1;i<coins.size();i++)
+        {
+            for(int j=1;j<=amount;j++)
+            {
+                long long take = 0, notTake = dp[i - 1][j];
+            if (coins[i] <= j)
+                take = dp[i][j - coins[i]];
+            dp[i][j] = static_cast<int>(take + notTake);
+            }
+        }
+        return dp[coins.size()-1][amount];
     }
 };
