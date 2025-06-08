c453bbab30cec0be4611d4b879c9cd4204b38727
shreelekha24 <2023ugee027@nitjsr.ac.in>
2025-06-08 18:12:08 +0530

    Time: 32 ms (55.50%) | Memory: 44.3 MB (44.58%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -17,28 +17,30 @@ int solve(vector<int>&coins,int amount,int i,vector<vector<int>>&dp)
     return dp[i][amount]=min(one,two);
 }
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>>dp(coins.size(),vector<int>(amount+1,0));
+    //    vector<vector<int>>dp(coins.size(),vector<int>(amount+1,0));
         int n=coins.size();
       //  int count=solve(coins,amount,0,dp);
+      vector<int>dp(amount+1,0);
         for(int i=0;i<=amount;i++)
         {
-            if(i%coins[n-1]==0) dp[n-1][i]=i/coins[n-1];
-            else dp[n-1][i]=INT_MAX;
+            if(i%coins[n-1]==0) dp[i]=i/coins[n-1];
+            else dp[i]=INT_MAX;
         }
         for(int i=n-2;i>=0;i--)
         {
+            vector<int>temp(amount+1);
             for(int j=1;j<=amount;j++)
             {
                int one=INT_MAX;
-               if(coins[i]<=j && dp[i][j-coins[i]]!=INT_MAX)
+               if(coins[i]<=j && temp[j-coins[i]]!=INT_MAX)
                {
-                one=dp[i][j-coins[i]]+1;
+                one=temp[j-coins[i]]+1;
                 }
-                int two=dp[i+1][j];
-                dp[i][j]=min(one,two);
-
+                int two=dp[j];
+                temp[j]=min(one,two);
             }
+            dp=temp;
         }
-        return dp[0][amount]==INT_MAX?-1:dp[0][amount];
+        return dp[amount]==INT_MAX?-1:dp[amount];
     }
 };
