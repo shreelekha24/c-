b2dac5aa85794c4b78264715aba3c3b956682204
shreelekha24 <2023ugee027@nitjsr.ac.in>
2025-06-10 16:28:23 +0530

    Time: 11 ms (63.53%) | Memory: 16.7 MB (57.06%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -9,7 +9,14 @@ int solve(string word1,string word2,int i,int j,vector<vector<int>>&dp){
     return dp[i][j]=max(solve(word1,word2,i-1,j,dp),solve(word1,word2,i,j-1,dp));
 }
     int minDistance(string word1, string word2) {
-        vector<vector<int>>dp(word1.size()+1,vector<int>(word2.size()+1,-1));
-        return word1.size()+word2.size()-2*solve(word1,word2,word1.size(),word2.size(),dp);
+        vector<vector<int>>dp(word1.size()+1,vector<int>(word2.size()+1,0));
+        for(int i=1;i<=word1.size();i++){
+            for(int j=1;j<=word2.size();j++)
+            {
+                if(word1[i-1]==word2[j-1]) dp[i][j]=1+dp[i-1][j-1];
+                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return word1.size()+word2.size()-2*dp[word1.size()][word2.size()];
     }
 };
