ccee5515fdf5d98d42c52c0d25e7bacba6f6ccac
shreelekha24 <2023ugee027@nitjsr.ac.in>
2025-06-11 16:47:41 +0530

    Time: 8 ms (46.09%) | Memory: 12.4 MB (83.70%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -14,25 +14,25 @@ int solve(string word1,string word2,int i,int j,vector<vector<int>>&dp)
        return dp[i][j]=min({1+solve(word1,word2,i+1,j+1,dp),1+solve(word1,word2,i,j+1,dp),1+solve(word1,word2,i+1,j,dp)});
 }
     int minDistance(string word1, string word2) {
-        vector<vector<int>>dp(word1.size()+1,vector<int>(word2.size()+1,0));
+       // vector<vector<int>>dp(word1.size()+1,vector<int>(word2.size()+1,0));
+       vector<int>dp(word2.size()+1,0);
         for(int i=0;i<=word2.size();i++)
         {
-            dp[0][i]=i;
-        }
-        for(int i=0;i<=word1.size();i++)
-        {
-            dp[i][0]=i;
+            dp[i]=i;
         }
         for(int i=1;i<=word1.size();i++)
         {
+            vector<int>temp(word2.size()+1);
+            temp[0]=i;
             for(int j=1;j<=word2.size();j++)
             { 
                 if(word1[i-1]==word2[j-1])
-                 dp[i][j]=dp[i-1][j-1];
+                 temp[j]=dp[j-1];
                 else
-                dp[i][j]=min({1+dp[i-1][j-1],1+dp[i][j-1],1+dp[i-1][j]});
+                temp[j]=min({1+dp[j-1],1+temp[j-1],1+dp[j]});
            }
+           dp=temp;
         }
-        return dp[word1.size()][word2.size()];
+        return dp[word2.size()];
     }
 };
