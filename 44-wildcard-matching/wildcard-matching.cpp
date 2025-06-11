c5e4e31f022b03953623302cf5ff9babc1da014b
shreelekha24 <2023ugee027@nitjsr.ac.in>
2025-06-11 18:04:05 +0530

    Time: 25 ms (81.48%) | Memory: 11.9 MB (84.81%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -23,8 +23,9 @@ bool solve(string s, string p,int i,int j,vector<vector<int>>&dp)
     return dp[i][j]=false;
 }
     bool isMatch(string s, string p) {
-        vector<vector<bool>>dp(s.size()+1,vector<bool>(p.size()+1,false));
-        dp[0][0]=true;
+      //  vector<vector<bool>>dp(s.size()+1,vector<bool>(p.size()+1,false));
+      vector<bool>dp(p.size()+1,false);
+        dp[0]=true;
         for(int i=1;i<=p.size();i++){
             bool flag=true;
             for(int k=1;k<=i;k++)
@@ -34,21 +35,23 @@ bool solve(string s, string p,int i,int j,vector<vector<int>>&dp)
                 break;
             }
         }
-        dp[0][i]=flag;
+        dp[i]=flag;
         }
         for(int i=1;i<=s.size();i++){
+            vector<bool>temp(p.size()+1);
             for(int j=1;j<=p.size();j++){
                 if(s[i-1]==p[j-1] || p[j-1]=='?'){
-                dp[i][j]=dp[i-1][j-1];
+                temp[j]=dp[j-1];
                 }
                 else if(p[j-1]=='*')
                 {
-                dp[i][j]=dp[i-1][j] || dp[i][j-1] ;
+                temp[j]=dp[j] || temp[j-1] ;
                 }
                 else
-                dp[i][j]=false;
+                temp[j]=false;
             }
+            dp=temp;
         }
-        return dp[s.size()][p.size()];
+        return dp[p.size()];
     }
 };
