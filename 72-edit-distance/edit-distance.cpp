c7734eecd56d5d94fb515ecadc586b834a48e10d
shreelekha24 <2023ugee027@nitjsr.ac.in>
2025-06-11 16:31:15 +0530

    Time: 11 ms (30.56%) | Memory: 13.1 MB (44.45%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -7,7 +7,6 @@ int solve(string word1,string word2,int i,int j,vector<vector<int>>&dp)
     if(j==word2.size())
     return word1.size()-i;
     if(dp[i][j]!=-1) return dp[i][j];
-    int one=0,two=0,three=0,four=0;
     if(word1[i]==word2[j])
     {
          return dp[i][j]=solve(word1,word2,i+1,j+1,dp);
@@ -15,7 +14,25 @@ int solve(string word1,string word2,int i,int j,vector<vector<int>>&dp)
        return dp[i][j]=min({1+solve(word1,word2,i+1,j+1,dp),1+solve(word1,word2,i,j+1,dp),1+solve(word1,word2,i+1,j,dp)});
 }
     int minDistance(string word1, string word2) {
-        vector<vector<int>>dp(word1.size()+1,vector<int>(word2.size()+1,-1));
-        return solve(word1,word2,0,0,dp);
+        vector<vector<int>>dp(word1.size()+1,vector<int>(word2.size()+1,0));
+        for(int i=0;i<=word2.size();i++)
+        {
+            dp[0][i]=i;
+        }
+        for(int i=0;i<=word1.size();i++)
+        {
+            dp[i][0]=i;
+        }
+        for(int i=1;i<=word1.size();i++)
+        {
+            for(int j=1;j<=word2.size();j++)
+            { 
+                if(word1[i-1]==word2[j-1])
+                 dp[i][j]=dp[i-1][j-1];
+                else
+                dp[i][j]=min({1+dp[i-1][j-1],1+dp[i][j-1],1+dp[i-1][j]});
+           }
+        }
+        return dp[word1.size()][word2.size()];
     }
 };
