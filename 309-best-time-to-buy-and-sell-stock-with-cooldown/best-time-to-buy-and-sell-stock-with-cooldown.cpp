e497fb12b91c949a88a9eb49eab72acb7bee1187
shreelekha24 <2023ugee027@nitjsr.ac.in>
2025-06-12 13:41:44 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (22.18%) - LeetSync

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+int solve(vector<int>&prices,int i,int buy,vector<vector<int>>&dp){
+    if(i>=prices.size()) return 0;
+    if(dp[i][buy]!=-1) return dp[i][buy];
+    if(buy){
+        dp[i][buy]=max(-prices[i]+solve(prices,i+1,0,dp),solve(prices,i+1,1,dp));
+    }else{
+        dp[i][buy]=max(prices[i]+solve(prices,i+2,1,dp),solve(prices,i+1,0,dp));
+    }
+    return dp[i][buy];
+}
+    int maxProfit(vector<int>& prices) {
+        vector<vector<int>>dp(prices.size()+1,vector<int>(2,-1));
+        return solve(prices,0,1,dp);
+    }
+};
