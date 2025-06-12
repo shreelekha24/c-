4c270e80c49ce9d2e17ed1da5feb2d83d2bae851
shreelekha24 <2023ugee027@nitjsr.ac.in>
2025-06-12 12:04:16 +0530

    Time: 4 ms (73.75%) | Memory: 17.1 MB (62.54%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -13,22 +13,25 @@ int solve(vector<int>& prices,int count,int i,int k,vector<vector<int>>&dp){
     return dp[i][count];
 }
     int maxProfit(int k, vector<int>& prices) {
-        vector<vector<int>>dp(prices.size()+1,vector<int>(2*k+1,0));
+       // vector<vector<int>>dp(prices.size()+1,vector<int>(2*k+1,0));
+       vector<int>dp(2*k+1,0);
         for(int i=prices.size()-1;i>=0;i--){
+            vector<int>temp(2*k+1,0);
             for(int count=2*k-1;count>=0;count--){
                 if(count%2==0){
-                int buy = (count + 1 <= 2 * k) ? -prices[i] + dp[i + 1][count + 1] : 0;
-                int skip = dp[i + 1][count];
-                dp[i][count] = max(buy, skip);
+                int buy = (count + 1 <= 2 * k) ? -prices[i] + dp[count + 1] : 0;
+                int skip = dp[count];
+                temp[count] = max(buy, skip);
                 }
                 else
                 {
-                int sell = (count + 1 <= 2 * k) ? prices[i] + dp[i + 1][count + 1] : 0;
-                int skip = dp[i + 1][count];
-                dp[i][count] = max(sell, skip);
+                int sell = (count + 1 <= 2 * k) ? prices[i] + dp[count + 1] : 0;
+                int skip = dp[count];
+                temp[count] = max(sell, skip);
                 }
             }
+            dp=temp;
         }
-        return dp[0][0];
+        return dp[0];
     }
 };
