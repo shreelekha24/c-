bfc8a7232ec7dc343ba363e88956ce9c73af53a7
shreelekha24 <2023ugee027@nitjsr.ac.in>
2025-06-14 01:31:29 +0530

    Time: 1191 ms (26.49%) | Memory: 198.8 MB (27.29%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -26,7 +26,7 @@ int solve(vector<string>&words,int prev,int i,vector<vector<int>>&dp)
             }
         }
         if(yes)
-            one = max(two, 1 + solve(words, i, i + 1,dp));
+            one = 1 + solve(words, i, i + 1,dp);
     }
     return dp[i][prev+1]=max(one,two);
 }
@@ -35,7 +35,35 @@ int longestStrChain(vector<string>& words) {
     sort(words.begin(), words.end(), [](const string &a, const string &b) {
     return a.size() < b.size();
 });
-vector<vector<int>>dp(words.size(),vector<int>(words.size()+1,-1));
-    return solve(words, -1, 0,dp);
+    vector<vector<int>>dp(words.size()+1,vector<int>(words.size()+1,0));
+        for(int i=words.size()-1;i>=0;i--){
+            for(int prev=i-1;prev>=-1;prev--){
+                int one=0,two=0;
+                two=dp[i+1][prev+1];
+               if(prev == -1 || words[prev].size() < words[i].size())
+               {
+                  bool yes = true;
+                  if(prev > -1)
+                  { 
+                   string s = words[i];
+                   yes = false;
+                   for(int j = 0; j < words[i].size(); j++)
+                    {
+                     string temp = s;
+                       temp.erase(j, 1); // FIX HERE
+                       if(temp == words[prev])
+                       {
+                        yes = true;
+                        break;
+                       }
+                    }
+                }
+                if(yes)
+                 one =  1 + dp[i+1][i + 1];
+               }
+               dp[i][prev+1]=max(one,two);
+            }
+        }
+        return dp[0][0];
 }
 };
