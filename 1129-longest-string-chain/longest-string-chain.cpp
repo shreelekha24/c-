6389b903bbd7f0a04b981aeb0e0b15f79dbf433d
shreelekha24 <2023ugee027@nitjsr.ac.in>
2025-06-14 01:12:20 +0530

    Time: 1540 ms (25.61%) | Memory: 200.4 MB (27.29%) - LeetSync

---
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+int solve(vector<string>&words,int prev,int i,vector<vector<int>>&dp)
+{
+    if(i==words.size())
+        return 0;
+    if(dp[i][prev+1]!=-1) return dp[i][prev+1];
+    int two = solve(words, prev, i + 1,dp);
+    int one =0;
+    if(prev == -1 || words[prev].size() < words[i].size())
+    {
+        bool yes = true;
+        if(prev > -1)
+        { 
+            string s = words[i];
+            yes = false;
+            for(int j = 0; j < words[i].size(); j++)
+            {
+                string temp = s;
+                temp.erase(j, 1); // FIX HERE
+                if(temp == words[prev])
+                {
+                    yes = true;
+                    break;
+                }
+            }
+        }
+        if(yes)
+            one = max(two, 1 + solve(words, i, i + 1,dp));
+    }
+    return dp[i][prev+1]=max(one,two);
+}
+
+int longestStrChain(vector<string>& words) {
+    sort(words.begin(), words.end(), [](const string &a, const string &b) {
+    return a.size() < b.size();
+});
+vector<vector<int>>dp(words.size(),vector<int>(words.size()+1,-1));
+    return solve(words, -1, 0,dp);
+}
+};
