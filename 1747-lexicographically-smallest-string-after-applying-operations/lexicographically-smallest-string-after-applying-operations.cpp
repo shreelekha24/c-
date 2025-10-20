f528ef9826e2a8fae4f63339344f4a4c9bf845d6
shreelekha24 <2023ugee027@nitjsr.ac.in>
2025-10-20 17:56:20 +0530

    Time: 207 ms (62.89%) | Memory: 103.1 MB (28.52%) - LeetSync

--- 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.cpp
+++ 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
     string findLexSmallestString(string s, int a, int b) {
-        unordered_set<string> vis;
-        string smallest = s;
-        queue<string> q;
-        q.push(s);
-        vis.insert(s);
+    unordered_set<string> vis;
+    queue<string> q;
+    q.push(s);
+    string smallest = s;
+    vis.insert(s);
+    
+    while (!q.empty()) {
+        string cur = q.front();
+        q.pop();
+        if (cur < smallest) smallest = cur;
 
-        while (!q.empty()) {
-            string cur = q.front(); q.pop();
-            if (cur < smallest) smallest = cur;
-
-            string added = cur;
-            for (int i = 1; i < added.size(); i += 2)
-                added[i] = ((added[i] - '0' + a) % 10) + '0';
-            if (!vis.count(added)) {
-                vis.insert(added);
-                q.push(added);
-            }
-
-            string rotated = cur.substr(cur.size() - b) + cur.substr(0, cur.size() - b);
-            if (!vis.count(rotated)) {
-                vis.insert(rotated);
-                q.push(rotated);
-            }
+        // Add operation on odd indices
+        string add = cur;
+        for (int i = 1; i < cur.size(); i += 2) {
+            add[i] = ((add[i] - '0' + a) % 10) + '0';
         }
-        return smallest;
+        if (!vis.count(add)) {
+            vis.insert(add);
+            q.push(add);
+        }
+
+        // Rotate operation
+        string p = cur.substr(cur.size() - b) + cur.substr(0, cur.size() - b);
+        if (!vis.count(p)) {
+            vis.insert(p);
+            q.push(p);
+        }
     }
+    return smallest;
+}
+
 };
